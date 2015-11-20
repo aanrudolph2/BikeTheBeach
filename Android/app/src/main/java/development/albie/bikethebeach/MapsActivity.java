4f182004a9c92ceb06e551d4ab835de96b77d221
justinrlok <justinrlok@gmail.com>
2015-11-20 11:12:35 -0500

    Displays the route on the map
    Displays the route on the map based on the coordinates given. (Messy at
    the moment)

--- Android/app/src/main/java/development/albie/bikethebeach/MapsActivity.java
+++ Android/app/src/main/java/development/albie/bikethebeach/MapsActivity.java
@@ -1,5 +1,6 @@
 package development.albie.bikethebeach;
 
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -9,12 +10,16 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, DataFetchable{
 
     private GoogleMap mMap;
+    public ArrayList<LatLng> polylines = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,8 +46,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         String data = "";
         for(int i =0; i< routes.get(0).getCoords().size();i++){
             data += routes.get(0).getCoords().get(i).getLat()+", "+routes.get(0).getCoords().get(i).getLongi()+"\n";
+            //double lat = routes.get(0).getCoords().get(i).getLat();
+            //double longi = routes.get(0).getCoords().get(i).getLongi();
+            polylines.add(new LatLng(routes.get(0).getCoords().get(i).getLat(),routes.get(0).getCoords().get(i).getLongi()));
+            //polylines.add(new LatLng(lat,longi));
+            //System.out.println("LAT" + routes.get(0).getCoords().get(i).getLat());
         }
         tv.setText(data);
+        createRoute(mMap);
     }
 
     /**
@@ -57,11 +68,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         LatLng sydney = new LatLng(39.71056240000001, -75.12029530000001);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Rowan"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(sydney.latitude, sydney.longitude), 15.0f));
+        LatLng test = new LatLng(39.7111543617113, -75.12760716460377);
+        mMap.addMarker(new MarkerOptions().position(test).title("Marker in Rowan"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(test));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(test.latitude, test.longitude), 15.0f));
+
+        //mMap.addPolyline(new PolylineOptions().addAll(polylines).width(15.0f).color(Color.RED));
+
+        //PolylineOptions opts = new PolylineOptions().addAll(polylines).width(5).color(Color.BLUE);
+        //mMap.addPolyline(opts);
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(test,39));
+        //polylines.add(new LatLng(test.latitude,test.longitude));
+        //System.out.println("WTF" + polylines.toString());
     }
 
+    public void createRoute(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng test = new LatLng(39.7111543617113, -75.12760716460377);
+        System.out.println("POLYLINES" + polylines.toString());
+        PolylineOptions opts = new PolylineOptions().addAll(polylines).width(5).color(Color.BLUE);
+        mMap.addPolyline(opts);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(test, 39));
+    }
 }
