04cb8abdf08a1f631bc059d9accf1f0bf639bedc
albiedoe <rynkie18@gmail.com>
2015-11-23 13:52:40 -0500

    Added Route Selection Menu with hard coded mapPreview on route selected. Need to change to pass in appropriate route data.

---
+++ Android/app/src/main/java/development/albie/bikethebeach/activities/RouteListActivity.java
@@ -0,0 +1,93 @@
+package development.albie.bikethebeach.activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
+
+import development.albie.bikethebeach.R;
+import development.albie.bikethebeach.routedata.DataAcquisition;
+import development.albie.bikethebeach.routedata.DataFetchable;
+import development.albie.bikethebeach.routedata.Route;
+import development.albie.bikethebeach.routedata.RouteListAdapter;
+
+/**
+ * Created by Albert on 11/23/2015.
+ */
+public class RouteListActivity extends FragmentActivity implements DataFetchable {
+
+    private Context mContext;
+    public ArrayList<LatLng> polylines = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_routes_list);
+        mContext = this;
+        DataAcquisition da = new DataAcquisition(this);
+        da.getRoutes();
+    }
+
+    /*
+     * Because data fetching is asyncronous, this is the callback function that gets called after calling da.getRoutes();
+     */
+    public void fetchRoutesFinish(ArrayList<Route> routes)
+    {
+        if(routes==null || routes.size() ==0)
+        {
+            System.out.println("Error in downloaded Route data.");
+            return;
+        }
+
+        for(int i =0; i< routes.get(0).getCoords().size();i++){
+            polylines.add(new LatLng(routes.get(0).getCoords().get(i).getLat(),routes.get(0).getCoords().get(i).getLongi()));
+            RouteListAdapter adapter = new RouteListAdapter(mContext, routes);
+            ListView list = (ListView) findViewById(R.id.routesList);
+            list.setAdapter(adapter);
+            setListener(list);
+        }
+
+    }
+
+    public void setListener(ListView lv){
+
+        //This is where we can create the modal for edit  delete
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Start the CAB using the ActionMode.Callback defined above
+                Toast.makeText(mContext, String.valueOf(position), Toast.LENGTH_SHORT).show();
+                RouteListActivity act = (RouteListActivity) mContext;
+                act.startPreview();
+                view.setSelected(true);
+
+                //THIS NEEDS TO PASS IN THE APPROPRIATE ROUTE DATA TO THE PREVIEW ACT
+            }
+        });
+
+    }
+
+    public void startPreview(){
+        Intent mapIntent = new Intent(this, MapsActivity.class);
+        this.startActivity(mapIntent);
+    }
+
+}
---
+++ Android/app/src/main/java/development/albie/bikethebeach/routedata/RouteListAdapter.java
@@ -0,0 +1,64 @@
+package development.albie.bikethebeach.routedata;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Currency;
+
+import development.albie.bikethebeach.R;
+import development.albie.bikethebeach.activities.RouteListActivity;
+
+
+public class RouteListAdapter extends BaseAdapter {
+    private final Context context;
+    private ArrayList<Route> routes;
+    public RouteListAdapter(Context context, ArrayList<Route> routes) {
+        this.context = context;
+        this.routes = routes;
+    }
+
+    @Override
+    public int getCount() {
+        return routes.size();
+    }
+
+    @Override
+    public Object getItem(int pos) {
+        return routes.get(pos);
+    }
+
+    @Override
+    public long getItemId(int pos) {
+        return 0l;
+        //just return 0 if your list items do not have an Id variable.
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.row_route_list, parent, false);
+
+        TextView routeNameTv= (TextView) rowView.findViewById(R.id.routeName);
+
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+
+        routeNameTv.setText("Route "+ String.valueOf(position));
+
+
+
+        return rowView;
+    }
+
+
+} 
