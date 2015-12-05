7afd95af53c56ce7f2182d347b1bbdfd818f4822
KevinPoretti <kevinporetti@gmail.com>
2015-12-04 20:13:49 -0500

    Route list now passes appropriate data to maps activity

--- Android/app/src/main/java/development/albie/bikethebeach/routedata/Route.java
+++ Android/app/src/main/java/development/albie/bikethebeach/routedata/Route.java
@@ -1,5 +1,8 @@
 package development.albie.bikethebeach.routedata;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.ArrayList;
 
 import development.albie.bikethebeach.routedata.Coord;
@@ -7,21 +10,50 @@ import development.albie.bikethebeach.routedata.Coord;
 /**
  * Created by Albert on 11/13/2015.
  */
-public class Route {
+public class Route implements Parcelable {
 
     private ArrayList<Coord> points;
     private int number;
 
-    public Route( ArrayList<Coord> points, int number){
+    public Route(ArrayList<Coord> points, int number) {
         this.points = points;
         this.number = number;
     }
 
-    public ArrayList<Coord> getCoords(){
+    protected Route(Parcel in) {
+        points = new ArrayList<Coord>();
+        in.readTypedList(points, Coord.CREATOR);
+        number = in.readInt();
+    }
+
+    public ArrayList<Coord> getCoords() {
         return points;
     }
 
-    public int getNum(){
+    public int getNum() {
         return number;
     }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeTypedList(points);
+        dest.writeInt(number);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Route> CREATOR = new Creator<Route>() {
+        @Override
+        public Route createFromParcel(Parcel in) {
+            return new Route(in);
+        }
+
+        @Override
+        public Route[] newArray(int size) {
+            return new Route[size];
+        }
+    };
 }
